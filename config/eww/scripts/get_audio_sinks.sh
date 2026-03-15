0e1658e7e3c669acf17e5c39008a9cddefa35f98
AleksL04 <sasha.likhoshva@gmail.com>
2026-03-15 15:20:05 -0400

    remove old files

--- config/eww/scripts/get_audio_sinks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Use pactl to get sink-inputs, and jq to format as JSON
-pactl -f json list sink-inputs | jq -c '[.[] | {
-    index: .index, 
-    name: .properties."application.name", 
-    volume: ((.volume | to_entries[0].value.value_percent) | sub("%"; "") | tonumber), 
-    muted: .mute
-}]'
--- config/eww/scripts/toggle_window.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-WINDOW="$1"
-
-if eww active-windows | grep -q "$WINDOW"; then
-    eww close "$WINDOW"
-else
-    eww open "$WINDOW"
-fi
