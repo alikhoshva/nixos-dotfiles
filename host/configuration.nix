f57e01f4d27c14e5b379f33c0ca36c5e22eac8fe
AleksL04 <sasha.likhoshva@gmail.com>
2026-04-14 00:12:33 -0400

    feat: enhance PipeWire configuration with crackling fixes for improved audio performance

--- host/configuration.nix
+++ host/configuration.nix
@@ -115,15 +115,32 @@
   ];
 
   # Enable PipeWire and WirePlumber for audio
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
-    jack.enable = true;
+    jack.enable = false;
     wireplumber.enable = true;
   };
 
-  security.rtkit.enable = true;
+  services.pipewire.extraConfig.pipewire = {
+    "98-crackling-fix" = {
+      "context.properties" = {
+        "default.clock.quantum" = 1024;
+        "default.clock.min-quantum" = 1024;
+        "default.clock.max-quantum" = 8192;
+      };
+    };
+  };
+
+  # additional fix for very bad devices or VM. 
+  services.pipewire.wireplumber.extraConfig = {
+    "99-crackling-fix" = {
+      "api.alsa.period-size" = 1024;
+      "api.alsa.headroom" = 8192;
+    };
+  };
 
   services.avahi = {
     enable = true;
