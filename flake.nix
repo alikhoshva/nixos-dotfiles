7e6426de97406151be8cda4f3f8538513f03273a
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-03 13:15:09 -0400

    feat(flake): add curd input and package with dependencies

--- flake.nix
+++ flake.nix
@@ -28,6 +28,10 @@
       url = "github:jacopone/antigravity-nix";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
+    curd = {
+      url = "github:Wraient/curd";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
--- homeManagerModules/system/flake-apps.nix
+++ homeManagerModules/system/flake-apps.nix
@@ -7,9 +7,14 @@ in
   home.packages = with inputs; [
     anipy-cli.packages.${system}.default
     nvim-config.packages.${system}.default
+    curd.packages.${system}.default
     #inputs.antigravity-nix.packages.${system}.google-antigravity-no-fhs
     antigravity-nix.packages.${system}.google-antigravity-ide
     #inputs.antigravity-nix.packages.${system}.google-antigravity-cli
+
+    # curd dependencies
+    pkgs.rofi
+    pkgs.ueberzugpp
   ];
 
 }
