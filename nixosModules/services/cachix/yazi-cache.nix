692da72552fe506fa23fd5966439600c1cb4297c
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-17 09:50:10 -0400

    nixosModules/services/cachix: remove walker, yazi, hyprland, noctalia caches

--- nixosModules/services/cachix/yazi-cache.nix
+++
@@ -1,7 +0,0 @@
-{
-  nix.settings = {
-    extra-substituters = [ "https://yazi.cachix.org" ];
-    extra-trusted-public-keys =
-      [ "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=" ];
-  };
-}
