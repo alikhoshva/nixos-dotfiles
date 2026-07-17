6019bad5cd8dc9543b6f23d7f1db82fb75c250dd
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-17 09:43:19 -0400

    chore: update nh to unstable and enable directory truncation to repo in starship

--- homeManagerModules/cli/nh.nix
+++ homeManagerModules/cli/nh.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }: {
+{ pkgs, pkgs-unstable, ... }: {
   programs.nh = {
     enable = true;
+    package = pkgs-unstable.nh;
     clean.enable = true;
     clean.extraArgs = "--keep-since 7d --keep 5";
     flake = "/home/aleks/nixos-dotfiles"; # sets NH_OS_FLAKE variable for you
