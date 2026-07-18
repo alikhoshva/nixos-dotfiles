bb96d3f1631ec4b7352df2cc88b9c3edcd3f98f0
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-18 15:04:23 -0400

    feat: add udev rules for Everglide SU75 Pro WebHID support

---
+++ nixosModules/hardware/keyboard.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+
+{
+  # udev rules for Everglide SU75 Pro keyboard WebHID support
+  services.udev.extraRules = ''
+    # Everglide SU75 Pro WebHID access
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1ca6", ATTRS{idProduct}=="3002", MODE="0666", TAG+="uaccess"
+  '';
+}
