3e0f8735d3f972d20f4a7887a08ff17fdd1d2162
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-25 11:12:08 -0400

    fix(nh): enable system-wide programs.nh in NixOS and set FLAKE=/home/aleks/nixos-dotfiles

--- homeManagerModules/cli/nh.nix
+++ homeManagerModules/cli/nh.nix
@@ -1,22 +1,23 @@
 { pkgs, ... }:
 
-let
-  nhScript = pkgs.writeShellApplication {
-    name = "nh";
-    runtimeInputs = with pkgs; [
-      git
-      nix-output-monitor
-      nvd
-    ];
-    text = builtins.readFile ./../../config/scripts/nh.sh;
-  };
-in
 {
-  home.sessionVariables = {
-    FLAKE = "/home/aleks/nixos-dotfiles";
-    NH_OS_FLAKE = "/home/aleks/nixos-dotfiles";
+  programs.nh = {
+    enable = true;
+    flake = "/home/aleks/nixos-dotfiles";
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 4d --keep 3";
+    };
   };
 
-  home.packages = [ nhScript ];
+  home.sessionVariables = {
+    FLAKE = "/home/aleks/nixos-dotfiles";
+    NH_FLAKE = "/home/aleks/nixos-dotfiles";
+  };
+
+  home.packages = with pkgs; [
+    nvd
+    nix-output-monitor
+  ];
 }
 
