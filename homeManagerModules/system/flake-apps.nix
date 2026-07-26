1ab07de4b988f69f11c53d57d130f658f6238188
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-26 13:59:19 -0400

    feat: add noctalia package to home configuration

--- homeManagerModules/system/flake-apps.nix
+++ homeManagerModules/system/flake-apps.nix
@@ -3,10 +3,10 @@ let
   system = pkgs.stdenv.hostPlatform.system;
 in
 {
-
   home.packages = with inputs; [
     #anipy-cli.packages.${system}.default
     nvim-config.packages.${system}.default
+    noctalia.packages.${system}.default
     #curd.packages.${system}.default
     #inputs.antigravity-nix.packages.${system}.google-antigravity-no-fhs
     antigravity-nix.packages.${system}.google-antigravity-ide
