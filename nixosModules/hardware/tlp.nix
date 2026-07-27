b53ef0b62948ca209f847e1acf3e7f83c4ed3c29
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-27 13:52:52 -0400

    refactor: update tlp power management settings and battery thresholds for improved amd efficiency

--- nixosModules/hardware/tlp.nix
+++ nixosModules/hardware/tlp.nix
@@ -2,20 +2,32 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      TLP_ENABLE = 1;
+
+      # CPU scaling governor (for amd-pstate-epp powersave is recommended for both AC & BAT)
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
+      # Energy Performance Preference (EPP) for AMD CPPC / HW P-States
       CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
+      # ACPI Platform Profiles (ThinkPad firmware profiles)
       PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "balanced";
 
-      CPU_MAX_PERF_ON_BAT = 40;
+      # AMD CPU Core Boost (enabled on AC, disabled on BAT for power efficiency)
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
 
-      #Optional helps save long term battery health
-      START_CHARGE_THRESH_BAT0 = 50; # 40 and below it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 95; # 80 and above it stops charging
+      # ThinkPad Battery Health Protection
+      START_CHARGE_THRESH_BAT0 = 60;
+      STOP_CHARGE_THRESH_BAT0 = 95;
+
+      # Power management for PCI Express and Wi-Fi
+      PCIE_ASPM_ON_BAT = "powersave";
+      WIFI_PWR_ON_BAT = "on";
     };
   };
 }
+
