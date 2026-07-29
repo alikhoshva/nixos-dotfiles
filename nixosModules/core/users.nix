49b63b0fcac454febade80a1bd80a8f403d02814
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-29 13:57:45 -0400

    feat: configure subUid and subGid ranges for the primary user

--- nixosModules/core/users.nix
+++ nixosModules/core/users.nix
@@ -7,5 +7,17 @@
       "networkmanager"
       "wheel"
     ];
+    subUidRanges = [
+      {
+        startUid = 100000;
+        count = 65536;
+      }
+    ];
+    subGidRanges = [
+      {
+        startGid = 100000;
+        count = 65536;
+      }
+    ];
   };
 }
