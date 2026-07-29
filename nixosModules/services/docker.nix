6cb87f5f3325c14ebc57a3038287b02ba96755c7
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-29 12:25:39 -0400

    feat: configure full podman-docker compatibility and add container development tools

--- nixosModules/services/docker.nix
+++ nixosModules/services/docker.nix
@@ -1,7 +1,26 @@
-{ ... }: {
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-  };
-}
+{ pkgs, ... }: {
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
 
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Enable Docker socket emulation so docker-compose works seamlessly
+      dockerSocket.enable = true;
+
+      # Required for containers under podman-compose/docker-compose to talk to each other
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  # Useful development tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    docker-compose # standard compose CLI (works with Podman via dockerSocket)
+    podman-compose # podman-native compose tool
+  ];
+}
