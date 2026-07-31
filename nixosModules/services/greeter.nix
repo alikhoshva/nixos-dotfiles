5f4d760b3a0fbf61d7e027fd600a44df6becb160
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-31 10:34:39 -0400

    chore: update tuigreet command to use uwsm for Hyprland session startup

--- nixosModules/services/greeter.nix
+++ nixosModules/services/greeter.nix
@@ -5,7 +5,7 @@
       # This block runs the login screen (tuigreet) BEFORE you log in.
       # It does not and should not mention Hyprland.
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd \"uwsm start hyprland-uwsm.desktop\"";
         user = "greeter";
       };
     };
