50bf61d75fd6c183e646483ef0e03a5643bd1f5f
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-31 10:15:56 -0400

    feat: improve session environment initialization and add restart policies for walker and elephant services

--- config/hypr/conf/autostart.lua
+++ config/hypr/conf/autostart.lua
@@ -9,6 +9,8 @@
 
 hl.on("hyprland.start", function () 
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+    hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+    hl.exec_cmd("systemctl --user start graphical-session.target")
     hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
     hl.exec_cmd("awww-daemon")
