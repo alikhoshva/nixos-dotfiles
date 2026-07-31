80d0d7ec7335da65e9417d91a28b6cde11129c35
AleksL04 <sasha.likhoshva@gmail.com>
2026-07-31 10:27:09 -0400

    refactor: migrate Hyprland session management to UWSM and remove redundant systemd service configurations

--- homeManagerModules/desktop/walker.nix
+++ homeManagerModules/desktop/walker.nix
@@ -15,17 +15,6 @@
     runAsService = true;
   };
 
-  # Auto-restart elephant & walker services on disconnect/failure
-  systemd.user.services.elephant.Service = {
-    Restart = lib.mkForce "always";
-    RestartSec = lib.mkForce "1s";
-  };
-
-  systemd.user.services.walker.Service = {
-    Restart = lib.mkForce "always";
-    RestartSec = lib.mkForce "1s";
-  };
-
   # Override Walker module's generated config files with our out-of-store symlinks
   xdg.configFile."walker/style.css".source = config.lib.file.mkOutOfStoreSymlink "/home/aleks/nixos-dotfiles/config/walker/style.css";
   xdg.configFile."walker/config.toml".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "/home/aleks/nixos-dotfiles/config/walker/config.toml");
--- nixosModules/programs/hyprland.nix
+++ nixosModules/programs/hyprland.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }: {
-  # Enable the Hyprland window manager
+  # Enable the Hyprland window manager with UWSM session management
   programs.hyprland = {
     enable = true;
     package = pkgs.unstable.hyprland;
+    withUWSM = true;
   };
 }
 
