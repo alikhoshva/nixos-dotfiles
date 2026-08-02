b172868bc881025b58107acb46ffdb345a4dfc2e
AleksL04 <sasha.likhoshva@gmail.com>
2026-08-02 10:52:45 -0400

    refactor: update dbus activation environment command to include all variables

--- config/hypr/conf/autostart.lua
+++ config/hypr/conf/autostart.lua
@@ -8,7 +8,7 @@
 -- Or execute your favorite apps at launch like this:
 
 hl.on("hyprland.start", function () 
-    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+    hl.exec_cmd("dbus-update-activation-environment --systemd --all")
     hl.exec_cmd("systemctl --user start hyprland-session.target")
     hl.exec_cmd("awww-daemon")
     hl.exec_cmd("noctalia-shell")
