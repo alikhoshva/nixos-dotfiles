23477473a327be37a8fe693df3fa71ee776ee399
AleksL04 <sasha.likhoshva@gmail.com>
2026-08-07 11:02:38 -0400

    style(scaling): adjust 4K desktop scale factor from 1.5 to 1.25 for comfortable UI density

--- config/scripts/set_display_mode.sh
+++ config/scripts/set_display_mode.sh
@@ -52,7 +52,7 @@ if [[ -n "$PRESET" ]]; then
     desktop-4k)
       OUTPUT="HDMI-A-1"
       MODE="3840x2160@120"
-      SCALE="1.5"
+      SCALE="1.25"
       POSITION="0x0"
       DISABLE="eDP-1"
       ;;
