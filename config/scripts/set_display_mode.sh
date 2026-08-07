3b7173bc1158bb3b429c7996b9eb615fb202a9cb
AleksL04 <sasha.likhoshva@gmail.com>
2026-08-07 11:17:09 -0400

    fix(scaling): revert scale factor to 1.333333 for Hyprland monitor alignment

--- config/scripts/set_display_mode.sh
+++ config/scripts/set_display_mode.sh
@@ -52,7 +52,7 @@ if [[ -n "$PRESET" ]]; then
     desktop-4k)
       OUTPUT="HDMI-A-1"
       MODE="3840x2160@120"
-      SCALE="1.4"
+      SCALE="1.333333"
       POSITION="0x0"
       DISABLE="eDP-1"
       ;;
