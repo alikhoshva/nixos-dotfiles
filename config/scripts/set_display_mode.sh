5556893fc1b99319fcde51bfe816a2782c35914d
AleksL04 <sasha.likhoshva@gmail.com>
2026-09-02 14:25:46 -0400

    chore: update default UI scale factor to 1.5 in display script and wrapper

--- config/scripts/set_display_mode.sh
+++ config/scripts/set_display_mode.sh
@@ -6,7 +6,7 @@
 
 OUTPUT="HDMI-A-1"
 MODE="3840x2160@120"
-SCALE="1.333333"
+SCALE="1.5"
 POSITION="0x0"
 DISABLE="eDP-1"
 PRESET=""
@@ -52,7 +52,7 @@ if [[ -n "$PRESET" ]]; then
     desktop-4k)
       OUTPUT="HDMI-A-1"
       MODE="3840x2160@120"
-      SCALE="1.333333"
+      SCALE="1.5"
       POSITION="0x0"
       DISABLE="eDP-1"
       ;;
