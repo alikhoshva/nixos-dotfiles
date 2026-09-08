27fbdfca9b625d3774d2c0eb85cfd6be115c2e2d
AleksL04 <sasha.likhoshva@gmail.com>
2026-09-08 13:11:13 -0400

    chore: remove ani-cli override to use default package version

--- homeManagerModules/system/latest.nix
+++ homeManagerModules/system/latest.nix
@@ -2,15 +2,7 @@
 
 {
   home.packages = with pkgs.unstable; [
-    (ani-cli.overrideAttrs (oldAttrs: rec {
-      version = "5.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "pystardust";
-        repo = "ani-cli";
-        tag = "v${version}";
-        hash = "sha256-rRQESi0Skoyf1jy/dRRK6ooKRPQhkak107kk5ulwZYI=";
-      };
-    }))
+    ani-cli
     devcontainer
     (yazi.override { _7zz = pkgs._7zz-rar; })
     #antigravity-fhs
